c09cdf991b38e910ae0d3c18521878f6e970c79e
Nitish Sarin <nitishhsarin@gmail.com>
2020-10-02 14:34:23 +0530

    Update 134.java
    more optimal

--- 134.java
+++ 134.java
@@ -1,27 +1,15 @@
 public int canCompleteCircuit(int[] gas, int[] cost) {
         int len = gas.length;
         int sum=0;
+        int minVal = Integer.MAX_VALUE;
+        int start = -1;
         for(int i = 0; i < gas.length; i++) {
-            gas[i]-=cost[i];
-            sum+=gas[i];
+            sum+=(gas[i]-cost[i]);
+            if(sum<minVal) {
+                minVal = sum;
+                start = (i+1)%len;
+            }
         }
         if(sum<0) return -1;
-        
-        for(int i = 0; i < gas.length; i++) {
-            int j = i;
-            int tcost = 0;
-            while(true) {
-                if((j+1)%len == i) {
-                    break;
-                }
-                tcost+=gas[j];
-                if(tcost<0) {
-                    i = j;
-                    break;
-                }
-                j = (j+1)%len;
-            }
-            if(tcost>=0) return i;
-        }
-        return 0;
+        return start;
     }
